62edf3bfdc2688081fcd66805895b87744fee28b
Ivan <154047253+IvanZ88@users.noreply.github.com>
2024-04-19 11:53:25 +0300

    Create cout_precision.cpp

---
+++ cout_precision.cpp
@@ -0,0 +1,20 @@
+﻿// cout_precision.cpp
+#include <iostream>
+#include <cstdlib>
+
+int main()
+{
+  using namespace std;
+
+  double a = 0, b = 0; // Числа с плавающей запятой.
+  cout << "a = ";
+  cin >> a;
+  cout << "b = ";
+  cin >> b;
+
+  cout.precision(16); // 16 значащих знаков.
+  cout << "quotient a:b  = " << a / b << endl;
+  //cout << "remainder a:b = " << a % b << endl;
+  // Операция взятия остатка не определена.
+  return EXIT_SUCCESS;
+}
